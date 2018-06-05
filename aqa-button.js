6f0f931330edfc51ca958219960703f2df2b0642
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-06-05 13:46:41 +0700

    update btn

--- aqa-button.js
+++ aqa-button.js
@@ -1,6 +1,6 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 // import '../../aqa-font/aqa-mitr-font.js';
-import '@aqa/aqa-font/aqa-mitr-font.js'
+import '@nylon/aqa-font/aqa-mitr-font.js'
 import '@polymer/iron-icon/iron-icon.js';
 import '@polymer/iron-icons/iron-icons.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
